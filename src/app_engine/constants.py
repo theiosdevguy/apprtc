b0a6a0a921a029040c913ee3404d7c5fc5e61e38
Ankit Gupta <theiosdevguy@gmail.com>
2016-07-11 17:28:26 +0530

    TURN details added

--- src/app_engine/constants.py
+++ src/app_engine/constants.py
@@ -12,8 +12,8 @@ MEMCACHE_RETRY_LIMIT = 100
 LOOPBACK_CLIENT_ID = 'LOOPBACK_CLIENT_ID'
 
 # TODO: Remove once clients support ICE_SERVER.
-TURN_BASE_URL = 'https://computeengineondemand.appspot.com'
-TURN_URL_TEMPLATE = '%s/turn?username=%s&key=%s'
+TURN_BASE_URL = 'https://turn:numb.viagenie.ca'
+TURN_URL_TEMPLATE = '?username=logik@darkdance.org'
 CEOD_KEY = '4080218913'
 
 ICE_SERVER_BASE_URL = 'https://networktraversal.googleapis.com'
