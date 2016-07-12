f7e9d385b261e0f8c333c4483f2d2de1b93eebec
Ankit Gupta <theiosdevguy@gmail.com>
2016-07-12 19:03:02 +0530

    Test

--- src/app_engine/constants.py
+++ src/app_engine/constants.py
@@ -12,11 +12,9 @@ MEMCACHE_RETRY_LIMIT = 100
 LOOPBACK_CLIENT_ID = 'LOOPBACK_CLIENT_ID'
 
 # TODO: Remove once clients support ICE_SERVER.
-TURN_BASE_URL = 'http://192.158.29.39:3478'
-TURN_URL_TEMPLATE = '%s?transport=tcp'
+TURN_BASE_URL = 'https://computeengineondemand.appspot.com'
+TURN_URL_TEMPLATE = '%s/turn?username=%s&key=%s'
 CEOD_KEY = '4080218913'
-USERNAME = 'sweepdevel@gmail.com'
-PASSWORD = 'tigerbright99'
 
 ICE_SERVER_BASE_URL = 'https://networktraversal.googleapis.com'
 ICE_SERVER_URL_TEMPLATE = '%s/v1alpha/iceconfig?key=%s'
